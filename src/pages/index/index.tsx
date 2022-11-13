9d96f9b30221ae89940e1926448aa5e2391a73d8
agions <1051736049@qq.com>
2022-11-13 12:00:57 +0800

    :toda: 项目初始化

---
+++ src/pages/index/index.tsx
@@ -0,0 +1,13 @@
+import { View, Text } from "@tarojs/components"
+import "./index.less"
+
+const Index = () => {
+  return (
+    <View className='index'>
+      <Text>Hello world!</Text>
+      {/* <Switch color='#000' excludedProps={["color"]} /> */}
+    </View>
+  )
+}
+
+export default Index
